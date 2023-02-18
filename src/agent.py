3fa6ae39f8fd4a567492c21d55a5cee14ceaa758
oschick <git@schicketanz.com.de>
2023-02-18 22:51:19 +0100

    Add option to use multiple meters

--- src/agent.py
+++ src/agent.py
@@ -10,19 +10,29 @@ if environ.get("UPDATE_TIME"):
 else:
     update_time = 5
 
+# Get the environment variables based on count of devices
+ # Strip leading and trailing whitespace
+sdm_types = [x.strip() for x in environ.get("SDM_TYPES").split(",")]
+print(sdm_types)
+# Strip leading and trailing whitespace from every element
+
+sdm_ids = [int(x.strip()) for x in environ.get("SDM_IDS").split(",")]
+print(sdm_ids)
+mqtt_sdm_topics = [x.strip() for x in environ.get("MQTT_SDM_TOPICS").split(",")]
+
 
 sdm_ip = environ.get('SDM_IP')
 sdm_port = int(environ.get('SDM_PORT'))
-sdm_type = environ.get('SDM_TYPE')
-sdm_network_type = environ.get('SDM_NETWORK_TYPE')
-sdm_id = int(environ.get('SDM_ID'))
+# sdm_type = environ.get('SDM_TYPE')
+sdm_network_type = environ.get('SDM_NETWORK_TYPE').lower()
+# sdm_id = int(environ.get('SDM_ID'))
 
 mqtt_broker_ip = environ.get('MQTT_BROKER_IP')
 mqtt_broker_port = int(environ.get('MQTT_BROKER_PORT'))
 mqtt_broker_auth = environ.get('MQTT_BROKER_AUTH')
 if mqtt_broker_auth:
     mqtt_broker_auth = json.loads(mqtt_broker_auth)
-mqtt_meter_topic = environ.get('MQTT_SDM_TOPIC')
+# mqtt_meter_topic = environ.get('MQTT_SDM_TOPIC')
 
 
 def publish_message(topic, data, ip, port, auth):
@@ -59,25 +69,39 @@ def connect_to_device(ip, port, network_type, device_type, device_id):
     else:
         raise Exception("Unknown network type: " + network_type)
 
+def connect_to_parent(parent, device_id, device_type):
+    dev = getattr(sdm_modbus, device_type)
+    return dev(parent=parent, device_id=device_id)
+
 def main():
     print ("starting...")
-    device = connect_to_device(sdm_ip, sdm_port, sdm_network_type.lower(), sdm_type, sdm_id)
-    print ("connected to device")        
+    devices = []
+    print ("connecting to device...")
+    devices.append(connect_to_device(sdm_ip, sdm_port, sdm_network_type, sdm_types[0], sdm_ids[0]))
+    print(devices[0])
+    if len(sdm_ids) > 1:
+        # Skip the first device, as it is already connected
+        for i in range(1, len(sdm_ids)):
+            devices.append(connect_to_parent(parent = devices[0], device_id=sdm_ids[i], device_type=sdm_types[i]))
+            print ("Connected to device: " + sdm_types[i] + " with Modbus ID: " + str(sdm_ids[i]))
+            print(devices[i])
+        print(len(devices)+ "Devices found.")
+    print ("connected to devices")
     while True:
         try:
-            if device.connected():
-                data = read_data(device)
-            else:
-                print ("failed to connect to sdm meter")
-                time.sleep(10)
-                continue
-            publish_message(topic=mqtt_meter_topic, data=data, ip=mqtt_broker_ip, port=mqtt_broker_port, auth=mqtt_broker_auth)
+            for i in range(len(devices)):
+                if devices[i].connected():
+                    data = read_data(devices[i])
+                    publish_message(mqtt_sdm_topics[i], data, mqtt_broker_ip, mqtt_broker_port, mqtt_broker_auth)
+                else:
+                    raise Exception(f"Device {sdm_ids[i]} is not connected")
             time.sleep(update_time)
 
         except Exception as ex:
             template = "An exception of type {0} occured. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             print (message)
+            time.sleep(5)
             continue
 
 main()
